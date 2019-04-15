3f3092750998162823aa4a148240eba4b9da0fe6
Calvin Spealman <cspealma@redhat.com>
2019-04-15 12:04:09 -0400

    Add description for package

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='findtext',
     version='0.1',
     description='Text finding helpers for Selenium tests',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/peaqe/findtext',
     author='Calvin Spealman',
     author_email='cspealma@redhat.com',
