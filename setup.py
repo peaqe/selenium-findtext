76aeb188d15cce847f8e82b4dd754db21e6eb5fa
Calvin Spealman <cspealma@redhat.com>
2019-08-21 09:20:14 -0400

    Update setup.py for 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='selenium-findtext',
-    version='0.1.3',
+    version='0.1.4',
     description='Text finding helpers for Selenium tests',
     long_description=long_description,
     long_description_content_type='text/markdown',
